51163317eb95396c2df99c61c0c557e9d71fe7d4
Eleazar <earicojr@gmail.com>
2024-01-18 21:33:46 -0800

    Finished Rollaball game

--- week0/Rollaball/Assets/Scripts/PlayerController.cs
+++ week0/Rollaball/Assets/Scripts/PlayerController.cs
@@ -3,28 +3,51 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour {
     public float speed = 1;
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update
     void Start() {
+        winTextObject.SetActive(false);
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
     }
 
-    void onMove (InputValue movementValue) {
+    void OnMove (InputValue movementValue) {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
 
-    private void FixedUpdate() {
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 8) {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate() {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("PickUp")) {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
     }
 }
