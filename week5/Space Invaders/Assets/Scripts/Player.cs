522584efb631c62ce8e5075b256e29f1f36a6aba
Eleazar <earicojr@gmail.com>
2024-03-08 22:11:27 -0800

    merge???

---
+++ week5/Space Invaders/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+  public GameObject bullet;
+  public Transform shootingOffset;
+  public int speed = 10;
+    // Update is called once per frame
+    void Update() {
+        float movement = Input.GetAxis("Horizontal");
+        Transform playerPos = GetComponent<Transform>();
+        Vector3 newPos = playerPos.position + Vector3.right * movement * Time.deltaTime * speed;
+        newPos.x = Mathf.Clamp(newPos.x, -17f, 17f);
+        playerPos.position = newPos;
+        
+        if (Input.GetKeyDown(KeyCode.Space)) { 
+            GameObject shot = Instantiate(bullet, shootingOffset.position, Quaternion.identity);
+            Debug.Log("Bang!");
+            Destroy(shot, 3f);
+            Debug.Log(playerPos.position);
+        }
+    }
+}
