2a89bf9b48d7e43dbe85511505ecfaa140fef367
eleazar <earicojr@gmail.com>
2024-03-12 17:17:54 -0700

    fixed merges and added enemy and player sprites

--- week5/Space Invaders/Assets/Scripts/Player.cs
+++ week5/Space Invaders/Assets/Scripts/Player.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,29 +22,3 @@ public class Player : MonoBehaviour {
         }
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Player : MonoBehaviour {
-  public GameObject bullet;
-  public Transform shootingOffset;
-  public int speed = 10;
-    // Update is called once per frame
-    void Update() {
-        float movement = Input.GetAxis("Horizontal");
-        Transform playerPos = GetComponent<Transform>();
-        Vector3 newPos = playerPos.position + Vector3.right * movement * Time.deltaTime * speed;
-        newPos.x = Mathf.Clamp(newPos.x, -17f, 17f);
-        playerPos.position = newPos;
-        
-        if (Input.GetKeyDown(KeyCode.Space)) { 
-            GameObject shot = Instantiate(bullet, shootingOffset.position, Quaternion.identity);
-            Debug.Log("Bang!");
-            Destroy(shot, 3f);
-            Debug.Log(playerPos.position);
-        }
-    }
-}
->>>>>>> ce4d47fd90af7d4f7deafa8d8b3d426c4dc5e5e5
--- week5/Space Invaders/Assets/UI/Scripts/Scroller.cs
+++ week5/Space Invaders/Assets/UI/Scripts/Scroller.cs
@@ -1,32 +1,13 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Scroller : MonoBehaviour
-{
+public class Scroller : MonoBehaviour {
     [SerializeField] private RawImage _img;
     [SerializeField] private float _x, _y;
 
-    void Update()
-    {
+    void Update() {
         _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _img.uvRect.size);
     }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class Scroller : MonoBehaviour
-{
-    [SerializeField] private RawImage _img;
-    [SerializeField] private float _x, _y;
-
-    void Update()
-    {
-        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _img.uvRect.size);
-    }
->>>>>>> ce4d47fd90af7d4f7deafa8d8b3d426c4dc5e5e5
 }
