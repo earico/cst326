42a6ea30de82e12b6bd5b45b692a8dc287486abd
eleazar <earicojr@gmail.com>
2024-04-10 23:42:47 -0700

    Tower Defense Tutorial pt. 3

--- week8/Tower Defense/Assets/Scripts/Enemy.cs
+++ week8/Tower Defense/Assets/Scripts/Enemy.cs
@@ -1,21 +1,29 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
     public float startSpeed = 10f;
     [HideInInspector]
     public float speed;
-    public float health = 100;
+    
+    public float startHealth = 100;
+    private float health;
     public int worth = 50;
     public GameObject deathEffect;
 
+    [Header("Unity Stuff")]
+    public Image healthBar;
+
     private void Start() {
         speed = startSpeed;
+        health = startHealth;
     }
 
     public void TakeDamage(float amount) {
         health -= amount;
-
+        healthBar.fillAmount = health / startHealth;
+        
         if (health <= 0) {
             Die();
         }
@@ -31,6 +39,9 @@ public class Enemy : MonoBehaviour {
         
         GameObject effect = (GameObject)Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(effect, 5f);
+
+        WaveSpawner.EnemiesAlive--;
+        
         Destroy(gameObject);
     }
 
